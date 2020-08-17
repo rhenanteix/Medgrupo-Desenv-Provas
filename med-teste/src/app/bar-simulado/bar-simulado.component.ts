2cecd85dcb8fde54ea3e2cb86b305329e20ca6d2
rhenan silverio <rhenan.teixeira@univision.net.br>
2020-08-17 20:42:47 -0300

    barra lateral

---
+++ med-teste/src/app/bar-simulado/bar-simulado.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bar-simulado',
+  templateUrl: './bar-simulado.component.html',
+  styleUrls: ['./bar-simulado.component.css']
+})
+export class BarSimuladoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}
